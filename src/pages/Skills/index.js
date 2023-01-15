48407cdcc255b3f9e741f5e246d81c727316f687
DuulgaEnkhtur <duulga16@gmail.com>
2023-01-15 22:05:41 +0800

    gert hiiv

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Styles from "./style.module.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import { Laptop } from "react-bootstrap-icons";
 
@@ -11,6 +14,15 @@ export default class Skills extends Component {
           <Laptop />
           Skills & Abilities
         </div>
+        <Container>
+          <Row>
+            <Col>1 of 1</Col>
+            <Col>1 of 1</Col>
+            <Col>1 of 1</Col>
+            <Col>1 of 1</Col>
+            <Col>1 of 1</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
